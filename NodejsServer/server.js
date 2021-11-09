3143208acae6ddb9dfcae0af9a36bbeb6bd4f0a8
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-11-10 03:17:10 +0530

    Update server.js

--- NodejsServer/server.js
+++ NodejsServer/server.js
@@ -71,5 +71,5 @@ app.post('/add', async (req, res) => {
  })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server listening at http://localhost:${port}`)
   })
