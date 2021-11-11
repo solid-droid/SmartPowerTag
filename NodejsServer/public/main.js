1551190e6b503c0d01e3ce41bbef0f1dd363dfa7
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-11-12 01:15:42 +0530

    updates

--- NodejsServer/public/main.js
+++ NodejsServer/public/main.js
@@ -1,21 +1,45 @@
 const getList = async () => await (await fetch('http://localhost:3300/getList')).json();
 const getData = async (id) => await (await fetch(`http://localhost:3300/getStatus/${id}`)).json();
-
+var myChart1, myChart2;
+var watt = [50, 10]
 const fullList = {};
 async function main() {
-    console.log("main");
-    $(".deviceList").empty();
     const list = (await getList()).data;
     list.forEach(async (id , index) => {
         const data = await getData(id);
         let duration = new Date() - new Date(data.time);
         duration = (((duration % 86400000) % 3600000) / 60000).toFixed(2); // minutes
-        fullList[id] = [{data: data.state, time: data.time, duration},...data.past];      
+        fullList[id] = [{data: data.state, time: data.time, duration},...data.past]; 
         if(index === list.length - 1) {
             await new Promise(resolve => setTimeout(resolve, 100));
+            $(".deviceList").empty();
+
+            myChart1.data.labels = [];
+            myChart2.data.labels = [];
+   
+            let OnTimeList = [];
             Object.keys(fullList).forEach((device,idx) => {
+                myChart1.data.labels.push('Device '+idx+' ');
+                myChart2.data.labels.push('Device '+idx+' ');
+                
+                const OnTime = fullList[device].filter(x =>  x.state == 'ON' || x.data == 'ON')
+                                            .map( x => parseFloat(x.duration))
+                                            .reduce((a , b) => a + b);
+
+                OnTimeList.push(OnTime);
+
                 buildDevice(device.replaceAll('_',':'), fullList[device][0].data, 'Device '+idx, fullList[device][0].duration);
             });
+
+            const total = OnTimeList.reduce((a,b)=>a+b);
+            let wattTime = OnTimeList.map((x,i) => x*watt[i]);
+            const totalWatt = wattTime.reduce((a,b)=>a+b);
+            OnTimeList = OnTimeList.map(x => Math.round(100*x/total));
+            wattTime = wattTime.map(x => Math.round(100*x/totalWatt));
+            myChart1.data.datasets[0].data= OnTimeList;
+            myChart2.data.datasets[0].data= wattTime;
+            myChart1.update();
+            myChart2.update();
         }
     });
 
@@ -62,22 +86,52 @@ const init = () => {
         $('.btn2').addClass("active");
     });
 
-    const ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    const ctx1 = document.getElementById('myChart1').getContext('2d');
+    myChart1 = new Chart(ctx1, {
         type: 'pie',
         data: {
-        labels: ["Green", "Blue", "Gray", "Purple", "Yellow", "Red", "Black"],
+        labels: [],
+
         datasets: [{
             backgroundColor: [
-            "#2ecc71",
-            "#3498db",
-            "#95a5a6",
-            "#9b59b6",
-            "#f1c40f",
-            "#e74c3c",
-            "#34495e"
-            ],
-            data: [12, 19, 3, 17, 28, 24, 7]
+                "#2ecc71",
+                "#3498db",
+                "#95a5a6",
+                "#9b59b6",
+                "#f1c40f",
+                "#e74c3c",
+                "#34495e"
+                ],
+            data: []
+        }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                display: false
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+        }
+    });
+
+    const ctx2 = document.getElementById('myChart2').getContext('2d');
+    myChart2 = new Chart(ctx2, {
+        type: 'pie',
+        data: {
+        labels: [],
+        datasets: [{
+            backgroundColor: [
+                "#2ecc71",
+                "#3498db",
+                "#95a5a6",
+                "#9b59b6",
+                "#f1c40f",
+                "#e74c3c",
+                "#34495e"
+                ],
+            data: []
         }]
         },
         options: {
@@ -91,7 +145,7 @@ const init = () => {
         }
     });
     main();
-    setInterval(main, 15000);
+    setInterval(main, 5000);
 }
 
 $(document).ready(init);
